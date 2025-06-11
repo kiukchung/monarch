6a7f5223f5ca0cb7ff25a967249680cd99e42acc
Shayne Fletcher <shaynefletcher@fb.com>
2025-06-10 23:25:47 -0700

    [hyperactor_mesh]: supervision: handle delivery failures via MeshAgent
    Pull Request resolved: https://github.com/pytorch-labs/monarch/pull/221
    
    
    ghstack-source-id: 289612445
    @exported-using-ghexport
    
    route undeliverable messages through mesh agent:
    - wire proc mailbox servers to forward undeliverables to the proc’s mesh agent (bootstrap.rs & local.rs)
    - extend `MeshAgent` to handle undeliverables by returning them to the originating sender (mesh_agent.rs)
    - covers cases where messages reach the proc but cannot be delivered locally
    - tested with local and process allocators (actor_mesh.rs)
    
    this lays initial scaffolding; further refinement of undeliverable handling will follow.
    
    Differential Revision: [D76388951](https://our.internmc.facebook.com/intern/diff/D76388951/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D76388951/)!

--- hyperactor_mesh/src/actor_mesh.rs
+++ hyperactor_mesh/src/actor_mesh.rs
@@ -356,6 +356,7 @@ pub(crate) mod test_util {
         Cast<Echo>,
         Cast<GetRank>,
         Cast<Error>,
+        GetRank,
         Relay,
         IndexedErasedUnbound<Cast<Echo>>,
         IndexedErasedUnbound<Cast<GetRank>>,
@@ -400,6 +401,20 @@ pub(crate) mod test_util {
             let ports = [self.1.port_id()];
             bindings.insert(ports)?;
             Ok(bindings)
+        }
+    }
+
+    #[async_trait]
+    impl Handler<GetRank> for TestActor {
+        async fn handle(
+            &mut self,
+            this: &Instance<Self>,
+            GetRank(ok, reply): GetRank,
+        ) -> Result<(), anyhow::Error> {
+            let rank = this.self_id().rank();
+            reply.send(this, rank)?;
+            anyhow::ensure!(ok, "intentional error!"); // If `!ok` exit with `Err()`.
+            Ok(())
         }
     }
 
@@ -692,6 +707,41 @@ mod tests {
                 for _ in 0..num_actors {
                     assert_eq!(rx.recv().await.unwrap(), CastTestMessage::Forward("abc".to_string()));
                 }
+            }
+
+            #[tokio::test]
+            async fn test_delivery_failure() {
+                use hyperactor::ActorId;
+                use hyperactor::ProcId;
+                use hyperactor::WorldId;
+                use hyperactor::mailbox::MessageEnvelope;
+                use hyperactor::mailbox::Undeliverable;
+
+                let alloc = $allocator
+                    .allocate(AllocSpec {
+                        shape: shape! { replica = 1  },
+                        constraints: Default::default(),
+                    })
+                    .await
+                    .unwrap();
+
+                let name = alloc.name().to_string();
+                let mesh = ProcMesh::allocate(alloc).await.unwrap();
+                let actor_mesh = mesh.spawn::<TestActor>("foo", &()).await.unwrap();
+                let reply_to = actor_mesh.open_port::<usize>().0.bind();
+                let (undeliverable_messages, mut undeliverable_rx) = actor_mesh.open_port::<Undeliverable<MessageEnvelope>>();
+                undeliverable_messages.bind_to(Undeliverable::<MessageEnvelope>::port());
+
+                // Send a message to a non-existent actor (the proc however exists).
+                let bad_actor = ActorRef::<TestActor>::attest(ActorId(ProcId(WorldId(name.clone()), 0), "foo".into(), 1));
+                bad_actor.send(mesh.client(), GetRank(true, reply_to)).unwrap();
+
+                // The message will be returned!
+                let Undeliverable(undelivered) = undeliverable_rx.recv().await.unwrap();
+                assert_eq!(mesh.client().actor_id(), undelivered.sender());
+                assert_eq!(&bad_actor.actor_id().port_id(GetRank::port()), undelivered.dest());
+
+                // TODO: Stop the proc.
             }
         }
     }
